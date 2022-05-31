145f412f29f287d8e6ea47c7ca437e80953eb0e9
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-05-31 19:26:52 -0400

    trimmed the whitespace, other solution was fine too though

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def csv_reader():
 
 
     ## USER INPUT
-    user_input = input("Please type in either cats or dogs: \n")
+    user_input = input("Please type in either cats or dogs: \n>>")
     file_test = 'data/' + user_input + ".csv"
     
     ## OPEN THE FILE (and handle bad input)
@@ -22,24 +22,26 @@ def csv_reader():
 
 
     dictionary = dict.fromkeys(animal_list[0])
-    final_list = []
-    output_list = []
+    # final_list = []
+    # output_list = []
 
     # for each animal in the list
     for x in range(1, len(animal_list)):
         # for each piece of information about the animal
         for i in range(len(animal_list[0])):
             # assign that information to the corresponding label in the dictionary
-            dictionary[animal_list[0][i]] = animal_list[x][i]
-
-        output_list.append(f"{animal_list[x][0]} is a{animal_list[x][1]} year old{animal_list[x][2]}.")
-        final_list.append(dictionary)
+            dictionary[animal_list[0][i].strip()] = animal_list[x][i].strip()
+        
+        # output strings for each animal
+        print(f"{animal_list[x][0].strip()} is a {animal_list[x][1].strip()} year old {animal_list[x][2].strip()}.")
+        #print(dictionary)
+        #output_list.append(f"{animal_list[x][0]} is a {(animal_list[x][1])} year old {animal_list[x][2]}.")
+        #final_list.append(dictionary)
 
     # display information about the animal
-    print(output_list)
+    # print(output_list)
 
     csvfile.close()
 
-    return False
 
 csv_reader()
