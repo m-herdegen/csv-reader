3b5c177f31fbd084d60242d6b032ee1961514c57
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 09:53:32 -0400

    using open, not with

--- main.py
+++ main.py
@@ -5,43 +5,48 @@ def csv_reader():
 
     ## USER INPUT
     user_input = input("Please type in either cats or dogs: \n>>")
-    file_test = 'data/' + user_input + ".csv"
+    file_test = './data/' + user_input + ".csv"
     
     ## OPEN THE FILE (and handle bad input)
     try:
-        open(file_test)
+        data = open(file_test)
+        reader = csv.DictReader(data)
+
+        for row in reader:
+            print(f"{row['name']} is a{row[' age']} year old{row[' breed']}.")
+            # print(row)
+
+        data.close()
     except:
         print(f"Sorry, we don't have any {user_input} here.")
 
     ## PARSE TO DATA STRUCTURE  
-    with open(file_test, newline='') as csvfile:
-        our_reader = csv.reader(csvfile, delimiter=',', quotechar = '|')
-        animal_list = []
-        for row in our_reader:
-            animal_list.append(row)
+    # with open(file_test, newline='') as csvfile:
+    #     our_reader = csv.reader(csvfile, delimiter=',', quotechar = '|')
+    #     animal_list = []
+    #     for row in our_reader:
+    #         animal_list.append(row)
 
 
-    dictionary = dict.fromkeys(animal_list[0])
+    # dictionary = dict.fromkeys(animal_list[0])
     # final_list = []
     # output_list = []
 
     # for each animal in the list
-    for x in range(1, len(animal_list)):
+    # for x in range(1, len(animal_list)):
         # for each piece of information about the animal
-        for i in range(len(animal_list[0])):
+        # for i in range(len(animal_list[0])):
             # assign that information to the corresponding label in the dictionary
-            dictionary[animal_list[0][i].strip()] = animal_list[x][i].strip()
+            # dictionary[animal_list[0][i].strip()] = animal_list[x][i].strip()
         
         # output strings for each animal
-        print(f"{animal_list[x][0].strip()} is a {animal_list[x][1].strip()} year old {animal_list[x][2].strip()}.")
+        # print(f"{animal_list[x][0].strip()} is a {animal_list[x][1].strip()} year old {animal_list[x][2].strip()}.")
         #print(dictionary)
         #output_list.append(f"{animal_list[x][0]} is a {(animal_list[x][1])} year old {animal_list[x][2]}.")
         #final_list.append(dictionary)
 
     # display information about the animal
     # print(output_list)
-
-    csvfile.close()
 
 
 csv_reader()
